d9f97d4b2c746723af4da97eb9db6f4088723573
vakhula <andrii.vakhula@gmail.com>
2024-11-02 12:23:21 +0100

    render data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
 'use client'
+import { useEffect, useState } from 'react';
 import { Canvas, useCanvas } from '@/app/canvas'
 
+interface Notice {
+  notice: string
+  x: number
+  y: number
+}
 export default function Home() {
+  const [notices, setNotices] = useState<Notice[]>([])
   const canvasRef = useCanvas()
+
+  useEffect(() => {
+    fetch('/api').then((resp) => {
+      resp.json().then((data: Notice[]) => {
+        setNotices(data)
+      })
+    })
+  }, [])
+
   return (
     <div>
-      <Canvas {...canvasRef}>hi</Canvas>
+      <Canvas {...canvasRef}>
+        {notices.map((item, idx) => (
+          <div
+            key={idx}
+            style={{
+              position: 'absolute',
+              top: item.y,
+              left: item.x,
+              backgroundColor: '#333',
+              padding: '1rem',
+              borderRadius: '1rem',
+              boxShadow: '6px 9px 24px -8px rgba(66, 68, 90, 1)',
+            }}
+          >
+            {item.notice}
+          </div>
+        ))}
+      </Canvas>
     </div>
   );
 }
